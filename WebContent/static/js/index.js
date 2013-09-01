2e8764b039d4f5a28ee3eb3ca311355141dfd034
airomega <airomega1@gmail.com>
2013-09-01 20:45:34 +0100

    working css switching

--- WebContent/static/js/index.js
+++ WebContent/static/js/index.js
@@ -7,12 +7,41 @@ $(document).ready(
 					function() {
 						var selectValue = $('#columnSelect :selected').val();
 						var div = document.createElement('div');
-						$(div).addClass("contentColumn").html(selectValue)
-								.appendTo($("#contentColumnDiv"));
+						$(div).addClass("contentColumn tertiary").html(
+								selectValue).appendTo($("#contentColumnDiv"));
 					});
 
-		});
+			$('#schemeButton').click(function() {
+				var selectValue = $('#schemeSelect :selected').val();
+				if (selectValue == 'blue') {
+					window.alert("blue");
+					updateStyleSheet('blue');
+				} else if (selectValue == 'yellow') {
+					window.alert("yellow");
+					updateStyleSheet('yellow');
+				} else {
+					window.alert("purple");
+					updateStyleSheet('purple');
+				}
+			});
 
-var getColumnCount = function() {
-	return columnCount++;
-};
+			function updateStyleSheet(filename) {
+
+				newstylesheet = "/StevesPlace/static/css/" + filename + ".css";
+
+				if ($("#dynamic_css").length == 0) {
+					$("head").append("<link>");
+					css = $("head").children(":last");
+					css.attr({
+						id : "dynamic_css",
+						rel : "stylesheet",
+						type : "text/css",
+						href : newstylesheet
+					});
+
+				} else {
+					$("#dynamic_css").attr("href", newstylesheet);
+				}
+
+			}
+		});
