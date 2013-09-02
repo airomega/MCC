ff50677c7f1af28d780630b72d9fa0be9e499b66
airomega <airomega1@gmail.com>
2013-09-02 22:53:47 +0100

    working video fade

--- WebContent/static/js/video.js
+++ WebContent/static/js/video.js
@@ -2,12 +2,18 @@ var index = 1;
 
 
 $(document).ready(function(){
-playlist = [ 'clover.mp4', 'fishing.mp4', 'insect.mp4','ruins.mp4', 'underwater.mp4', 'wasp.mp4', 'wheat.mp4' ];
+playlist = [ 'clover.mp4', 'fishing.mp4', 'insect.mp4','ruins.mp4', 'wasp.mp4', 'wheat.mp4' ];
 video = document.getElementById('backgroundVideo');
-video.addEventListener('ended', rotate_video, false);
+setInterval(rotate_video, 10000)
 });
 
 function rotate_video() {
+	$('#fader').animate({opacity: 1},800);
+	setTimeout(loadVideo,800);
+	$('#fader').animate({opacity: 0},1200);
+}
+
+function loadVideo(){
 	video.setAttribute('src', "/StevesPlace/static/video/" +playlist[index]);
 	video.load();
 	index++;
